d8e7523112c15ea478136988dd54c8797bff30ac
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-07 15:26:52 +0300

    refactor: move OnboardingStatus to new namespace

---
+++ src/Revolt/Models/Platform/Onboarding/OnboardingStatus.cs
@@ -0,0 +1,10 @@
+namespace Revolt.Models.Platform.Onboarding
+{
+    public class Status
+    {
+        /// <summary>
+        /// An alphanumeric username which is used to identify the user on the platform.
+        /// </summary>
+        public bool Onboarding { get; set; }
+    }
+}
