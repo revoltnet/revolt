77604415d21ef4ce5069de32937ab791641b4412
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-07 19:06:20 +0300

    feat: add RevoltException

---
+++ src/Revolt/RevoltException.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Revolt
+{
+    public class RevoltException : Exception
+    {
+        public RevoltException()
+        {
+        }
+
+        protected RevoltException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public RevoltException(string? message) : base(message)
+        {
+        }
+
+        public RevoltException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
