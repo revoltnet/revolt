77f795d232d8b4b90ef790f9b06ea3a3df610ddc
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-07 18:16:43 +0300

    feat: define platform client interface

--- src/Revolt/IPlatformClient.cs
+++ src/Revolt/IPlatformClient.cs
@@ -1,7 +1,21 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Revolt.Models.Platform.Core;
+
 namespace Revolt
 {
     public interface IPlatformClient
     {
-        
+        #region Core
+
+        Task<Node> QueryNodeAsync(CancellationToken cancellationToken);
+
+        #endregion
+
+        #region Onboarding
+
+        Task<bool> CheckOnboardingStatusAsync(CancellationToken cancellationToken);
+
+        #endregion
     }
 }
