10d1678628d1169a1f7c0764e3b9239d22cae125
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-07 17:17:29 +0300

    feat: define Account model

---
+++ src/Revolt/Models/Auth/Account.cs
@@ -0,0 +1,15 @@
+namespace Revolt.Models.Auth
+{
+    public class Account
+    {
+        /// <summary>
+        /// User ID.
+        /// </summary>
+        public string Id { get; set; }
+        
+        /// <summary>
+        /// User Email.
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
