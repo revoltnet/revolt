05c959b95c37993496845ab55f50aade2e69b055
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-07 17:26:04 +0300

    feat: define Profile model

---
+++ src/Revolt/Models/Users/Information/Profile.cs
@@ -0,0 +1,10 @@
+using Revolt.Models.Common;
+
+namespace Revolt.Models.Users.Information
+{
+    public class Profile
+    {
+        public string Content { get; set; }
+        public File Background { get; set; }
+    }
+}
