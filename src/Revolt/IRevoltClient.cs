67b99d17c8c741551d1335d7c71c4c27aefa1250
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-09 17:20:15 +0300

    docs: improve IRevoltClient documentation

--- src/Revolt/IRevoltClient.cs
+++ src/Revolt/IRevoltClient.cs
@@ -1,8 +1,13 @@
 namespace Revolt
 {
     /// <summary>
-    /// Fully featured Revolt client.
+    /// Fully featured Revolt API client.
+    /// Includes implementations for platform API, auth API.
     /// </summary>
+    /// <remarks>
+    /// See also <seealso cref="IPlatformClient"/>.
+    /// See also <seealso cref="IAuthClient"/>.
+    /// </remarks>
     public interface IRevoltClient : IPlatformClient, IAuthClient
     {
         
