39ff4170adb860501e3234bb5088bdf1e3c07aef
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-09 15:32:12 +0300

    docs: update documentation for IPlatformClient

--- src/Revolt/IPlatformClient.cs
+++ src/Revolt/IPlatformClient.cs
@@ -8,14 +8,33 @@ namespace Revolt
     {
         #region Core
 
+        /// <summary>
+        /// This returns information about which features are enabled on the remote node.
+        /// </summary>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>All <see cref="Node"/> information.</returns>
         Task<Node> QueryNodeAsync(CancellationToken cancellationToken = default);
 
         #endregion
 
         #region Onboarding
 
+        /// <summary>
+        /// This will tell you whether the current account requires onboarding or whether you can continue to send requests as usual.
+        /// </summary>
+        /// <remarks>
+        /// You may skip calling this if you're restoring an existing session.
+        /// </remarks>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>True if the account requires onboarding, false otherwise.</returns>
         Task<bool> CheckOnboardingStatusAsync(CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// This sets a new username, completes onboarding and allows a user to start using Revolt.
+        /// </summary>
+        /// <param name="username">New username.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns></returns>
         Task CompleteOnboarding(string username, CancellationToken cancellationToken = default);
 
         #endregion
