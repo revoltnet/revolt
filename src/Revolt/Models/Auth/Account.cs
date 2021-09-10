9f0318341cb045c77ad0b6752caea4135eb26576
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 14:50:43 +0300

    feat: add init to Account

--- src/Revolt/Models/Auth/Account.cs
+++ src/Revolt/Models/Auth/Account.cs
@@ -2,18 +2,21 @@ using System.Text.Json.Serialization;
 
 namespace Revolt.Models.Auth
 {
+    /// <summary>
+    /// User account.
+    /// </summary>
     public class Account
     {
         /// <summary>
         /// User ID.
         /// </summary>
         [JsonPropertyName("id")]
-        public string Id { get; }
+        public string Id { get; init; }
         
         /// <summary>
         /// User Email.
         /// </summary>
         [JsonPropertyName("email")]
-        public string Email { get; }
+        public string Email { get; init; }
     }
 }
