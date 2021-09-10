358c67f0ccbe2c353b46aaf74d8be639c246b199
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 10:57:10 +0300

    feat: improve Profile documentation and json properties

--- src/Revolt/Models/Users/Information/Profile.cs
+++ src/Revolt/Models/Users/Information/Profile.cs
@@ -1,10 +1,20 @@
+using System.Text.Json.Serialization;
 using Revolt.Models.Common;
 
 namespace Revolt.Models.Users.Information
 {
     public class Profile
     {
+        /// <summary>
+        /// Profile content.
+        /// </summary>
+        [JsonPropertyName("content")]
         public string Content { get; set; }
+        
+        /// <summary>
+        /// Profile background.
+        /// </summary>
+        [JsonPropertyName("background")]
         public File Background { get; set; }
     }
 }
