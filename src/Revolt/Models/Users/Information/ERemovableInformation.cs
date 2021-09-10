1b98bc5e32e2d6976d8441e3429081edde604b05
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 14:16:33 +0300

    feat: add ERemovableInformation

---
+++ src/Revolt/Models/Users/Information/ERemovableInformation.cs
@@ -0,0 +1,28 @@
+namespace Revolt.Models.Users.Information
+{
+    /// <summary>
+    /// Removable information.
+    /// </summary>
+    public enum ERemovableInformation
+    {
+        /// <summary>
+        /// Avatar.
+        /// </summary>
+        Avatar,
+        
+        /// <summary>
+        /// Profile background.
+        /// </summary>
+        ProfileBackground,
+        
+        /// <summary>
+        /// Profile context.
+        /// </summary>
+        ProfileContent,
+        
+        /// <summary>
+        /// Status text.
+        /// </summary>
+        StatusText
+    }
+}
