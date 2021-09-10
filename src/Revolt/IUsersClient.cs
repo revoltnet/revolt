57b176e94c6b61f996543d97e59bcf5503c313e5
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 16:03:09 +0300

    docs: update documentation for IUsersClient

--- src/Revolt/IUsersClient.cs
+++ src/Revolt/IUsersClient.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -17,7 +18,7 @@ namespace Revolt
         /// Retrieve a user's information.
         /// </summary>
         /// <param name="userId">User id.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns>An instance of <see cref="User"/>.</returns>
         Task<User> FetchUser(string userId, CancellationToken cancellationToken = default);
         
@@ -28,16 +29,16 @@ namespace Revolt
         /// <param name="profile">User profile data.</param>
         /// <param name="avatarId">Autumn file ID.</param>
         /// <param name="remove">Field to remove from user object.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns></returns>
-        Task EditUser(Status status, Profile profile, string avatarId, ERemovableInformation remove, CancellationToken cancellationToken = default);
+        Task EditUser(Status? status, Profile? profile, string? avatarId, ERemovableInformation? remove, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Change your username.
         /// </summary>
         /// <param name="newUsername">New username.</param>
         /// <param name="password">Current account password.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns></returns>
         Task ChangeUsername(string newUsername, string password, CancellationToken cancellationToken = default);
 
@@ -45,7 +46,7 @@ namespace Revolt
         /// Retrieve a user's profile data.
         /// </summary>
         /// <param name="userId">User id.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns>An instance of <see cref="Profile"/>.</returns>
         Task<Profile> FetchUserProfile(string userId, CancellationToken cancellationToken = default);
 
@@ -53,7 +54,7 @@ namespace Revolt
         /// This returns a default avatar based on the given id.
         /// </summary>
         /// <param name="userId">User id.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns>Default avatar in PNG format.</returns>
         Task<MemoryStream> FetchDefaultAvatar(string userId, CancellationToken cancellationToken = default);
 
@@ -61,7 +62,7 @@ namespace Revolt
         /// Retrieve a list of mutual friends with another user.
         /// </summary>
         /// <param name="userId">User id.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns>Collection of user IDs who both you and the other user are friends with.</returns>
         Task<IEnumerable<string>> FetchMutualFriends(string userId, CancellationToken cancellationToken = default);
 
@@ -72,30 +73,82 @@ namespace Revolt
         /// <summary>
         /// This fetches your direct messages, including any DM and group DM conversations.
         /// </summary>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
         /// <returns></returns>
         Task<IEnumerable<IChannel>> FetchDirectMessageChannels(CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Open a DM with another user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns></returns>
         Task<DirectMessage> OpenDirectMessage(string userId, CancellationToken cancellationToken = default);
 
         #endregion
 
         #region Relationships
 
+        /// <summary>
+        /// Fetch all of your relationships with other users.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Collection of <see cref="Relationship"/>.</returns>
         Task<IEnumerable<Relationship>> FetchAllRelationships(CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Fetch your relationship with another other user.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>An instance of <see cref="Relationship"/>.</returns>
         Task<Relationship> FetchRelationship(CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Send a friend request to another user.
+        /// </summary>
+        /// <param name="username">Username of the user.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>An instance of <see cref="Status"/>.</returns>
         Task<Status> SendFriendRequest(string username, CancellationToken cancellationToken = default);
         
+        /// <summary>
+        /// Accept another user's friend request.
+        /// </summary>
+        /// <param name="username">Username of the user.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>An instance of <see cref="Status"/>.</returns>
         Task<Status> AcceptFriendRequest(string username, CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Denies another user's friend request.
+        /// </summary>
+        /// <param name="username">Username of the user.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>An instance of <see cref="Status"/>.</returns>
         Task<Status> DenyFriendRequest(string username, CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Removes an existing friend.
+        /// </summary>
+        /// <param name="username">Username of the user.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>An instance of <see cref="Status"/>.</returns>
         Task<Status> RemoveFriend(string username, CancellationToken cancellationToken = default);
         
+        /// <summary>
+        /// Block another user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>An instance of <see cref="Status"/>.</returns>
         Task<Status> BlockUser(string userId, CancellationToken cancellationToken = default);
         
+        /// <summary>
+        /// Unblock another user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+        /// <returns>An instance of <see cref="Status"/>.</returns>
         Task<Status> UnblockUser(string userId, CancellationToken cancellationToken = default);
 
         #endregion
