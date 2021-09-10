28520ccd0b07f92886fe52e475e5e81695f3243c
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 11:06:30 +0300

    feat: improve Relation documentation and json properties

--- src/Revolt/Models/Users/Relationships/Relation.cs
+++ src/Revolt/Models/Users/Relationships/Relation.cs
@@ -1,8 +1,22 @@
+using System.Text.Json.Serialization;
+
 namespace Revolt.Models.Users.Relationships
 {
+    /// <summary>
+    /// Represents a relationship with a user.
+    /// </summary>
     public class Relation
     {
+        /// <summary>
+        /// Your relationship with the user.
+        /// </summary>
+        [JsonPropertyName("status")]
         public ERelationship Status { get; set; }
+        
+        /// <summary>
+        /// Other user's id.
+        /// </summary>
+        [JsonPropertyName("_id")]
         public string Id { get; set; }
     }
 }
