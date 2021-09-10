ed7749ef8edc933183ed872e05557c884bdf55d3
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 10:56:59 +0300

    feat: improve ERelationship documentation and json properties

--- src/Revolt/Models/Users/Relationships/ERelationship.cs
+++ src/Revolt/Models/Users/Relationships/ERelationship.cs
@@ -1,13 +1,43 @@
 namespace Revolt.Models.Users.Relationships
 {
+    /// <summary>
+    /// Possible user relationships.
+    /// </summary>
     public enum ERelationship
     {
+        /// <summary>
+        /// Blocked.
+        /// </summary>
         Blocked,
+        
+        /// <summary>
+        /// Blocked other.
+        /// </summary>
         BlockedOther,
+        
+        /// <summary>
+        /// Friend.
+        /// </summary>
         Friend,
+        
+        /// <summary>
+        /// Incoming.
+        /// </summary>
         Incoming,
+        
+        /// <summary>
+        /// None.
+        /// </summary>
         None,
+        
+        /// <summary>
+        /// Outgoing.
+        /// </summary>
         Outgoing,
+        
+        /// <summary>
+        /// User.
+        /// </summary>
         User
     }
 }
