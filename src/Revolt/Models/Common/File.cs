72bdf74fe5d97ecb2565dd5883a92912a2b64cbe
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 10:56:35 +0300

    feat: improve File documentation and json properties

--- src/Revolt/Models/Common/File.cs
+++ src/Revolt/Models/Common/File.cs
@@ -1,14 +1,47 @@
+using System.Text.Json.Serialization;
 using Revolt.Models.Users.Information;
 
 namespace Revolt.Models.Common
 {
+    /// <summary>
+    /// Represents a file, attachment, etc.
+    /// </summary>
     public class File
     {
+        /// <summary>
+        /// File id.
+        /// </summary>
+        [JsonPropertyName("_id")]
         public string Id { get; set; }
+        
+        /// <summary>
+        /// File tag.
+        /// </summary>
+        [JsonPropertyName("tag")]
         public string Tag { get; set; }
+        
+        /// <summary>
+        /// File size (in bytes)
+        /// </summary>
+        [JsonPropertyName("size")]
         public long Size { get; set; }
+        
+        /// <summary>
+        /// File name.
+        /// </summary>
+        [JsonPropertyName("filename")]
         public string Filename { get; set; }
+        
+        /// <summary>
+        /// File metadata.
+        /// </summary>
+        [JsonPropertyName("metadata")]
         public IMetadata Metadata { get; set; }
+        
+        /// <summary>
+        /// File content type.
+        /// </summary>
+        [JsonPropertyName("content_type")]
         public string ContentType { get; set; }
     }
 }
