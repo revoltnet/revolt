e5ece5aff2ab45a9a24641f7afc0828544840639
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 14:08:53 +0300

    feat: update DirectMessage

--- src/Revolt/Models/Users/DirectMessaging/DirectMessage.cs
+++ src/Revolt/Models/Users/DirectMessaging/DirectMessage.cs
@@ -1,15 +1,44 @@
+#nullable enable
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Revolt.Models.Channels.Information;
 
 namespace Revolt.Models.Users.DirectMessaging
 {
   
+    /// <summary>
+    /// Direct message.
+    /// </summary>
     public class DirectMessage : IChannel
     {
-        public string Id { get; }
-        public bool Active { get; set; }
-        public List<string> Recipients { get; set; }
-        public LastMessageData LastMessage { get; set; }
-        public string Nonce { get; }
+        /// <summary>
+        /// Channel Id.
+        /// </summary>
+        [JsonPropertyName("_id")]
+        public string Id { get; init; } = null!;
+        
+        /// <summary>
+        /// Whether this DM is active.
+        /// </summary>
+        [JsonPropertyName("active")]
+        public bool Active { get; init; }
+
+        /// <summary>
+        /// List of user IDs who are participating in this DM
+        /// </summary>
+        [JsonPropertyName("recipients")]
+        public List<string> Recipients { get; init; } = null!;
+
+        /// <summary>
+        /// Last message sent in channel.
+        /// </summary>
+        [JsonPropertyName("last_message")]
+        public LastMessage LastMessage { get; init; } = null!;
+        
+        /// <summary>
+        /// Nonce hash.
+        /// </summary>
+        [JsonPropertyName("nonce")]
+        public string? Nonce { get; init; }
     }
 }
