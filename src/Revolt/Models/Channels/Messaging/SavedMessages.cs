c327642d3debd0f93385ef90c6a2c5ec6e3663d9
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-10 15:23:07 +0300

    docs: add more info to SavedMessages

--- src/Revolt/Models/Channels/Messaging/SavedMessages.cs
+++ src/Revolt/Models/Channels/Messaging/SavedMessages.cs
@@ -1,5 +1,10 @@
+using Revolt.Models.Channels.Information;
+
 namespace Revolt.Models.Channels.Messaging
 {
+    /// <summary>
+    /// Saved messages channel.
+    /// </summary>
     public class SavedMessages : IChannel
     {
         /// <inheritdoc />
