bb0ca556b698fdf1957e91438be8281139257de6
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-15 16:32:57 +0200

    feat: move IPlatformClient implementation to partial class RevoltClient
    In order to make it easy to update later

---
+++ src/Revolt/RevoltClient.IPLatformClient.cs
@@ -0,0 +1,76 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Revolt.Models.Platform.Core;
+
+namespace Revolt
+{
+    public partial class RevoltClient
+    {
+        #region IPLatformClient
+
+        /// <inheritdoc />
+        public async Task<Node> QueryNodeAsync(CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync(string.Empty, cancellationToken).ConfigureAwait(false);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                throw new RevoltException(response.ReasonPhrase, e);
+            }
+
+            var node = await response.Content.ReadFromJsonAsync<Node>(cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            return node ?? throw new RevoltException("Something went wrong deserializing the response.");
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> CheckOnboardingStatusAsync(CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync("onboard/hello", cancellationToken).ConfigureAwait(false);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                throw new RevoltException(response.ReasonPhrase, e);
+            }
+
+            var content = await response.Content.ReadFromJsonAsync<JsonElement>(cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            if (content.TryGetProperty("onboarding", out content)) return content.GetBoolean();
+
+            throw new RevoltException("Something went wrong deserializing the response.");
+        }
+
+        /// <inheritdoc />
+        public async Task CompleteOnboardingAsync(string username, CancellationToken cancellationToken = default)
+        {
+            var payload = GeneratePayload(new
+            {
+                username
+            });
+
+            var response = await Client.PostAsync("onboard/complete", payload, cancellationToken).ConfigureAwait(false);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                throw new RevoltException(response.ReasonPhrase, e);
+            }
+        }
+
+        #endregion
+    }
+}
--- src/Revolt/RevoltClient.cs
+++ src/Revolt/RevoltClient.cs
@@ -20,7 +20,7 @@ using Revolt.Options;
 namespace Revolt
 {
     /// <inheritdoc />
-    public class RevoltClient : IRevoltClient
+    public partial class RevoltClient : IRevoltClient
     {
         /// <summary>
         ///     Options.
@@ -50,70 +50,6 @@ namespace Revolt
         {
             return new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json");
         }
-
-        #region IPLatformClient
-
-        /// <inheritdoc />
-        public async Task<Node> QueryNodeAsync(CancellationToken cancellationToken = default)
-        {
-            var response = await Client.GetAsync(string.Empty, cancellationToken).ConfigureAwait(false);
-
-            try
-            {
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException e)
-            {
-                throw new RevoltException(response.ReasonPhrase, e);
-            }
-
-            var node = await response.Content.ReadFromJsonAsync<Node>(cancellationToken: cancellationToken).ConfigureAwait(false);
-
-            return node ?? throw new RevoltException("Something went wrong deserializing the response.");
-        }
-
-        /// <inheritdoc />
-        public async Task<bool> CheckOnboardingStatusAsync(CancellationToken cancellationToken = default)
-        {
-            var response = await Client.GetAsync("onboard/hello", cancellationToken).ConfigureAwait(false);
-
-            try
-            {
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException e)
-            {
-                throw new RevoltException(response.ReasonPhrase, e);
-            }
-
-            var content = await response.Content.ReadFromJsonAsync<JsonElement>(cancellationToken: cancellationToken).ConfigureAwait(false);
-
-            if (content.TryGetProperty("onboarding", out content)) return content.GetBoolean();
-
-            throw new RevoltException("Something went wrong deserializing the response.");
-        }
-
-        /// <inheritdoc />
-        public async Task CompleteOnboardingAsync(string username, CancellationToken cancellationToken = default)
-        {
-            var payload = GeneratePayload(new
-            {
-                username
-            });
-
-            var response = await Client.PostAsync("onboard/complete", payload, cancellationToken).ConfigureAwait(false);
-
-            try
-            {
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException e)
-            {
-                throw new RevoltException(response.ReasonPhrase, e);
-            }
-        }
-
-        #endregion
 
         #region IAuthClient
 
